a32e5e7b470a39726075d84a257c54f32e068d82
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-07-30 22:27:30 +0530

    updated jobs list

--- schedule/views.py
+++ schedule/views.py
@@ -17,9 +17,43 @@ def get_jobs_list(parent):
     # Make the request
     page_result = client.list_jobs(request=request)
 
-    #Handle the response
+    # Process the response data
+    job_list = []
     for response in page_result:
-        return HttpResponse(response)
+        # Convert each job into a dictionary
+        job_data = { 
+            "name": response.name,
+            "http_method": response.http_method,
+            "http_target": response.http_target,
+            "schedule": response.schedule,
+            "time_zone": response.time_zone,
+            "state": response.state,
+            "user_update_time": response.user_update_time,
+            "status": response.status,
+            "schedule_time": response.schedule_time,
+            "last_attempt_time": response.last_attempt_time,
+            "retry_config": response.retry_config,
+            "attempt_deadline": response.attempt_deadline            
+            }
+        job_list.append(job_data)
+
+    # Return the response as JSON
+    return JsonResponse(job_list, safe=False)
+
+# def get_jobs_list(parent):
+
+#     # Create a client
+#     client = scheduler_v1.CloudSchedulerClient()
+
+#     # Initialize request argument(s)
+#     request = scheduler_v1.ListJobsRequest(parent="projects/gcp-training-386807/locations/us-central1")
+
+#     # Make the request
+#     page_result = client.list_jobs(request=request)
+
+#     #Handle the response
+#     for response in page_result:
+#         return HttpResponse(response)
 
 
 def sample_create_job():
