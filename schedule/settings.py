1cc9ed0e6b77f68266f86e40b1f41a9e20f93b2f
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-07-30 18:14:20 +0530

    updated settinsg file

--- schedule/settings.py
+++ schedule/settings.py
@@ -8,7 +8,7 @@ JSON_KEY_FILE_PATH = os.environ.get('JSON_KEY_FILE_PATH')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://django-scheduler-3imv474m7a-uc.a.run.app"]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8080',  # for localhost (Developlemt)
