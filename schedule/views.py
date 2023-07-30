c3a1fcfe0b648ad8c39b454c172ce1962f31f219
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-07-30 19:12:15 +0530

    updated jobs list

--- schedule/views.py
+++ schedule/views.py
@@ -11,11 +11,13 @@ def get_jobs_list(parent):
     client = scheduler_v1.CloudSchedulerClient()
 
     # Initialize request argument(s)
-    request = scheduler_v1.ListJobsRequest()
+    request = scheduler_v1.ListJobsRequest(parent="projects/gcp-training-386807/locations/us-central1")
 
     # Make the request
     page_result = client.list_jobs(request=request)
 
     # Handle the response
+    # for response in page_result:
+    #     return HttpResponse(response)
     response_data = [str(response) for response in page_result]
     return HttpResponse("\n".join(response_data))
